7686cc844e4a8cc24f09c257c916c693d303c988
Mrz <86775732+MrZ1922@users.noreply.github.com>
2023-07-30 22:13:18 +0300

    Create main.py
    Changes Will Be Made

---
+++ main.py
@@ -0,0 +1,13 @@
+import info
+
+print("Welcome To The Mrz Bank")
+while True:
+  print("\nChoose an action:\n1 -> Enter To The Account\n2 -> Create A New Account")
+  choise = int(input("?: "))
+  if choise == 1:
+    pass
+  elif choise == 2:
+    pass
+  else:
+    print("Error.Please Try Again")
+  
