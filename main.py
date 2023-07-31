f145ca971c5b807a0c398cae7ce6f5266145a1f5
Mrz <86775732+MrZ1922@users.noreply.github.com>
2023-07-31 18:50:42 +0300

    Update main.py
    Will Work On it

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import info
 
-print("Welcome To The Mrz Bank")
+print("\nWelcome To The Mrz Bank!\n")
+
 while True:
-  print("\nChoose an action:\n1 -> Enter To The Account\n2 -> Create A New Account")
-  choise = int(input("?: "))
-  if choise == 1:
-    pass
-  elif choise == 2:
-    pass
-  else:
-    print("Error.Please Try Again")
-  
+    print("Please select a action:\n1 -> Create new acount\n2 -> Enter acount")
+    choise = int(input("?: "))
+    if choise == 1:
+        pass
+    elif choise == 2:
+        while True:
+            cartNameIn = input("Please write cart holders name: ")
+            cartNumIn = int(input("Please write cart number: "))
+            cartPassIn = int(input("Please write cart password: "))
+            for crt in range(len(info.cartNames)):
+                if crt <= (len(info.cartNames)-1):
+                    if info.cartNum[crt] == cartNumIn and info.cartNames[crt] == cartNameIn and info.cartPass[crt] == cartPassIn:
+                        print("bus" + str(info.cartMoney[crt]))
+                        print("\nPlease select an option: \n")
+                        print("1 -> Withdraw\n2 -> Deposit\n3 -> Other Currencys\n 4 -x`> Exit")
+                        x = input("wait")
+                if crt + 1 == len(info.cartNames):    
+                    print("wait") 
+    else:
+        print("Error.Please try again")
